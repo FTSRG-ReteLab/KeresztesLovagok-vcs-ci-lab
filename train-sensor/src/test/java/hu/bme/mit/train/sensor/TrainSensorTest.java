14a8570a1c56224449f00de82289e5f90aa34631
ernoGitGudGitHub <fehererno4444@gmail.com>
2018-02-19 07:41:42 -0800

    Fix and Junit

--- train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
+++ train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
@@ -3,17 +3,34 @@ package hu.bme.mit.train.sensor;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+
+import hu.bme.mit.train.interfaces.TrainSensor;
+
+import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
 public class TrainSensorTest {
 
+	TrainSensor sensor;
+	int value1;
+    int value2;
     @Before
     public void before() {
+    	value1=2;
+    	value2=3;
         // TODO Add initializations
+    	 
     }
 
     @Test
     public void ThisIsAnExampleTestStub() {
         // TODO Delete this and add test cases based on the issues
     }
+    @Test
+    public void ThisIsANewTest() {
+        // TODO Delete this and add test cases based on the issues
+    	int result = value1 + value2;
+        assertTrue(result == 6);
+    }
+
 }
--- train-system/src/test/java/hu/bme/mit/train/system/TrainSystemTest.java
+++ train-system/src/test/java/hu/bme/mit/train/system/TrainSystemTest.java
@@ -50,5 +50,6 @@ public class TrainSystemTest {
 		Assert.assertEquals(0, controller.getReferenceSpeed());
 	}
 
+
 	
 }
