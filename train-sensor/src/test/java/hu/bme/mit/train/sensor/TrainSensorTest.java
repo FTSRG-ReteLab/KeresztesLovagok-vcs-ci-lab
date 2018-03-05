6c62e65cdd6f49e51a73e2be805a57941c6933d0
meres <szalontaijozsi@gmail.com>
2018-03-05 08:40:51 -0800

    utolso feladat

--- train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
+++ train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
@@ -4,7 +4,9 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import hu.bme.mit.train.interfaces.TrainController;
 import hu.bme.mit.train.interfaces.TrainSensor;
+import hu.bme.mit.train.interfaces.TrainUser;
 
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
@@ -14,14 +16,23 @@ public class TrainSensorTest {
 	TrainSensor sensor;
 	int value1;
     int value2;
+    TrainController mockController;
+    TrainUser mockUser;
+    
+    TrainSensorImpl trainSensor;
+    
     @Before
     public void before() {
     	value1=2;
     	value2=3;
         // TODO Add initializations
+    	
+    	mockController = mock(TrainController.class);
+    	mockUser = mock(TrainUser.class);
+    	trainSensor = new TrainSensorImpl(mockController, mockUser);	
     	 
     }
-
+/*
     @Test
     public void ThisIsAnExampleTestStub() {
         // TODO Delete this and add test cases based on the issues
@@ -31,6 +42,36 @@ public class TrainSensorTest {
         // TODO Delete this and add test cases based on the issues
     	int result = value1 + value2;
         assertTrue(result == 5);
+    }
+  */  
+    @Test
+    public void negativeSpeedLimit() {	
+    	trainSensor.overrideSpeedLimit(-1);
+    	verify(mockUser, times(1)).setAlarmState(true);   	
+   	
+    }
+    @Test
+    public void bigSpeedLimit() {	
+    	trainSensor.overrideSpeedLimit(501);
+    	verify(mockUser, times(1)).setAlarmState(true);   	
+   	
+    }
+    
+    @Test
+    public void smallerThanHalfRefSpeed() {
+    	when(mockController.getReferenceSpeed()).thenReturn(100);
+    	
+    	trainSensor.overrideSpeedLimit((int)(mockController.getReferenceSpeed()*0.4));
+    	verify(mockUser, times(1)).setAlarmState(true);   	
+   	
+    }
+    @Test
+    public void equalHalfRefSpeed() {	
+    	when(mockController.getReferenceSpeed()).thenReturn(100);
+    	
+    	trainSensor.overrideSpeedLimit((int)(mockController.getReferenceSpeed()*0.5));
+    	verify(mockUser, times(0)).setAlarmState(false);   	
+   	
     }
 
 }
